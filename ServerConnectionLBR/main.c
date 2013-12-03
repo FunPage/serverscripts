5c163d2c553a892b775e39711da4732a849ce949
FunPage <rajanfunpage@gmail.com>
2013-12-03 19:37:31 +0530

    server build unit

---
+++ ServerConnectionLBR/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sathyanarrayananstark
+ *
+ * Created on December 3, 2013, 7:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
